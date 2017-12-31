9e50d35d3a04eb681f1b15171fecfa13e9ef21aa
Sajjad AliBakhshi <you@example.com>
2017-12-31 14:04:14 +0330

    first commit 10.10.96 13:59

---
+++ music/views.py
@@ -0,0 +1,67 @@
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .models import Album
+
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+
+class DetailView(generic.DetailView):
+    template_name = 'music/detail.html'
+    model = Album
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = [
+        'artist', 'album_title', 'genre', 'album_logo'
+    ]
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = [
+        'artist', 'album_title', 'genre', 'album_logo'
+    ]
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy("music:index")
+
+# from django.http import Http404
+# from django.shortcuts import render, get_object_or_404
+# from .models import Album, Song
+#
+#
+# def index(request):
+#     all_albums = Album.objects.all()
+#     return render(request, 'music/index.html', {'all_albums': all_albums, })
+#
+#
+# def detail(request, album_id):
+#     # album = Album.objects.get(pk=album_id)
+#     album = get_object_or_404(Album, pk=album_id)
+#     return render(request, 'music/detail.html', {'album': album})
+#
+#
+# def favorite(request, album_id):
+#     album = get_object_or_404(Album, pk=album_id)
+#     try:
+#         selected_song = album.song_set.get(pk=request.POST['song'])
+#     except (KeyError, Song.DoesNotExist):
+#         return render(request, 'music/detail.html', {
+#             'album': album,
+#             'error_message': "You did not select a valid song",
+#         })
+#
+#     else:
+#         selected_song.is_favorite = True
+#         selected_song.save()
+#         return render(request, 'music/detail.html', {'album': album})
